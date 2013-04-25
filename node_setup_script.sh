174bbdfaa9a403ab63d06ba35f73c12a5f01a0af
Siddharth Choudhary <siddharthchoudhary.ri@gmail.com>
2013-04-25 14:45:56 -0700

    delete disabled

--- node_setup_script.sh
+++ node_setup_script.sh
@@ -4,9 +4,13 @@ target_url="target_url"
 
 sudo apt-get install apache2 httrack tshark
 
-httrack $target_url -O "~/websites/remote_site" --mirrorlinks  -%v -r4 -%e0 -#L100000
+httrack $target_url -O "~/websites/remote_site" --mirrorlinks  -%v -r4 -%e0 -#L1000
 
 sudo cp -r ~/websites/remote_site/* /var/www/
 
 git clone git@github.com:SiddharthC/CCrawler.git
 
+
+#set up the crawler and run it between durations of this 
+
+#sudo tshark -i lo -f "port 80" -a duration:60 -z conv,ip -q
